e3e7be72fb7b193f295a08f6db6f893a3464d98a
Joonho D. Kim <37599608+jkmolina@users.noreply.github.com>
2020-02-26 18:23:08 -0600

    Added shooting functionality, layers, tags and "respawn" option

---
+++ Gravity Dude/Assets/Scripts/Player1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+
+    public int hp = 100;
+    public GameObject jugador1;
+    public int xPosition;
+    public int yPosition;
+    Vector2 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 pos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void TakeDamage(int amount)
+    {
+        hp -= amount;
+        if (hp <= 0)
+        {
+            //Destroy(gameObject);
+            pos.x = xPosition;
+            pos.y = yPosition;
+            transform.position = pos;
+        }
+    }
+}
---
+++ Gravity Dude/Assets/Scripts/Player2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public int hp = 100;
+    public GameObject jugador2;
+    public int xPosition;
+    public int yPosition;
+    Vector2 pos;
+    void Start()
+    {
+        Vector2 pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(int amount)
+    {
+        hp -= amount;
+        if (hp <= 0)
+        {
+            pos.x = xPosition;
+            pos.y = yPosition;
+            transform.position = pos;
+        }
+    }
+}
